d043c46a805562dc65f9b233b70f06136a64b4fe
LUISPINTO90 <lpinto1@ucol.mx>
2022-10-16 17:08:10 -0500

    Add login and register validation

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 //  ARRAY USERS
-let users = [];
+let users = [{ name: "a", email: "a@a.com", password: "a" }];
 
 //  CLASS USER
 class User {
@@ -8,18 +8,6 @@ class User {
     this.email = email;
     this.password = password;
   }
-
-  getName() {
-    return this.name;
-  }
-
-  getEmail() {
-    return this.email;
-  }
-
-  getPassword() {
-    return this.password;
-  }
 }
 
 //  REGISTER USER ALERT
@@ -27,14 +15,16 @@ document.getElementById("btnRegister").addEventListener("click", () => {
   Swal.fire({
     customClass: {
       confirmButton: "swalBtnColor",
+      title: "swalTitle",
     },
     title: "REGÍSTRATE",
-    icon: "info",
     background: "#fff",
     html: `
-      <input style="margin:auto; margin-top: 1em;" type="text" id="userName" class="swal2-input" placeholder="Nombre">
-      <input style="margin:auto; margin-top: 1em;" type="text" id="userEmail" class="swal2-input" placeholder="Correo">
-      <input style="margin:auto; margin-top: 1em;" type="text" id="userPassWord" class="swal2-input" placeholder="Contraseña">
+      <form>
+        <input style="margin:auto; margin-top: 1em;" type="text" id="userName" class="swal2-input" placeholder="Nombre">
+        <input style="margin:auto; margin-top: 1em;" type="text" id="userEmail" class="swal2-input" placeholder="Correo">
+        <input style="margin:auto; margin-top: 1em; margin-bottom: 2em" type="text" id="userPassWord" class="swal2-input" placeholder="Contraseña">
+      </form>
     `,
     confirmButtonText: "Crear cuenta",
     focusConfirm: false,
@@ -43,17 +33,85 @@ document.getElementById("btnRegister").addEventListener("click", () => {
       let userEmail = Swal.getPopup().querySelector("#userEmail").value;
       let userPassWord = Swal.getPopup().querySelector("#userPassWord").value;
 
-      if (!userName || !userEmail || !userPassWord) {
+      emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+      if (
+        userName == "" ||
+        userEmail == "" ||
+        userPassWord == "" ||
+        !emailRegex.test(userEmail)
+      ) {
         Swal.showValidationMessage(
           `Por favor, ingrese los datos correctamente`
         );
+      } else {
+        let newUser = new User(userName, userEmail, userPassWord);
+
+        if (users.find((user) => user.email == userEmail)) {
+          Swal.showValidationMessage(`El correo ya est&aacute; en uso`);
+        } else {
+          users.push(newUser);
+
+          Swal.fire({
+            customClass: {
+              confirmButton: "swalBtnColor",
+              title: "swalTitle",
+            },
+            title: "REGISTRO EXITOSO",
+            icon: "success",
+          });
+
+          console.log(users);
+        }
       }
+    },
+  });
+});
 
-      let newUser = new User(userName, userEmail, userPassWord);
+//  LOGIN USER ALERT
+document.getElementById("btnLogin").addEventListener("click", () => {
+  Swal.fire({
+    customClass: {
+      confirmButton: "swalBtnColor",
+      title: "swalTitle",
+    },
+    title: "INICIA SESI&Oacute;N",
+    background: "#fff",
+    html: `
+      <form>
+        <input style="margin:auto; margin-top: 1em;" type="text" id="userEmail" class="swal2-input" placeholder="Correo">
+        <input style="margin:auto; margin-top: 1em; margin-bottom: 2em" type="text" id="userPassWord" class="swal2-input" placeholder="Contraseña">
+      </form>
+    `,
+    confirmButtonText: "Ingresar",
+    focusConfirm: false,
+    preConfirm: () => {
+      let userEmail = Swal.getPopup().querySelector("#userEmail").value;
+      let userPassWord = Swal.getPopup().querySelector("#userPassWord").value;
 
-      users.push(newUser);
+      emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
-      console.log(users);
+      if (
+        userEmail == "" ||
+        userPassWord == "" ||
+        !emailRegex.test(userEmail)
+      ) {
+        Swal.showValidationMessage(
+          `Por favor, ingrese los datos correctamente`
+        );
+      } else {
+        if (
+          users.find(
+            (user) => user.email == userEmail && user.password == userPassWord
+          )
+        ) {
+          window.location.href = "./profile.html";
+        } else {
+          Swal.showValidationMessage(
+            `Por favor, ingrese los datos correctamente`
+          );
+        }
+      }
     },
   });
 });
